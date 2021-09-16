2836d2a9b094a41ac22474ae768234a3fe6b0f71
devansh singh <66402296+mrcharasi@users.noreply.github.com>
2021-09-16 15:33:50 +0530

    Add files via upload

---
+++ CENTRAL PARK SQUIRREL CENSUS ANALYSIS/main.py
@@ -0,0 +1,15 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels, red_squirrels, black_squirrels]
+}
+
+data_frame = pandas.DataFrame(data_dict)
+data_frame.to_csv("squirrels_count.csv")
+print(data_frame)
